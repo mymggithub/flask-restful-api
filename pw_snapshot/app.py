99e95861d3387873f59e8507a3251610d2a493a6
Mark Garcia <mymgcode@gmail.com>
2022-10-17 16:59:25 -0500

    log info and flush print
    also fixed the mysql to leave if table empty

--- pw_snapshot/app.py
+++ pw_snapshot/app.py
@@ -10,7 +10,9 @@ import logging
 if not os.path.exists("log"):
 	os.mkdir('log');
 
-logging.basicConfig(filename="log/default.log", level=logging.DEBUG);
+logging.basicConfig(filename="log/default.log");
+logging.getLogger().setLevel(logging.DEBUG);
+logging.getLogger().setLevel(logging.INFO);
 
 DBconfig = {
 	"host":"mysql_db", 
@@ -19,7 +21,7 @@ DBconfig = {
 	"database":"yiiadv"
 };
 
-CONTINUE_LOOP = True;
+CONTINUE_LOOP = False;
 try:
 	time.sleep(5)
 	mydb = mysql.connector.connect(**DBconfig);
@@ -29,51 +31,62 @@ try:
 	mydb.close();
 	cont_loop = (myresult["settings_value"] == 1);
 	while cont_loop:
-		logging.info("Bio_snapshot: On");
+		msg = "Bio_snapshot: On";
+		logging.info(msg);
+		print(msg, flush=True);
 		time.sleep(1)
 		mydb = mysql.connector.connect(**DBconfig);
 		mycursor = mydb.cursor(dictionary=True);
 		mycursor.execute("SELECT `t_username` FROM `twitter`");
 		all_user_results = mycursor.fetchall();
 		mydb.close();
-		for i,x in enumerate(all_user_results):
-			u = x["t_username"];
-			if not os.path.exists("pics"):
-				os.mkdir('pics');
-			if not os.path.exists("pics/{}.png".format(u)):
-				with sync_playwright() as p:
-					browser = p.chromium.launch()
-					page = browser.new_page()
-					logging.info("Opening https://twitter.com/{}".format(u));
-					page.goto("https://twitter.com/{}".format(u))
-					page.wait_for_selector("img")
-					page.screenshot(path="pics/{}.png".format(u))
-					browser.close()
+		if len(all_user_results):
+			for i,x in enumerate(all_user_results):
+				u = x["t_username"];
+				if not os.path.exists("pics"):
+					os.mkdir('pics');
+				if not os.path.exists("pics/{}.png".format(u)):
+					with sync_playwright() as p:
+						browser = p.chromium.launch()
+						page = browser.new_page()
+						msg = "Opening https://twitter.com/{}".format(u);
+						logging.info(msg);
+						print(msg, flush=True);
+						page.goto("https://twitter.com/{}".format(u))
+						page.wait_for_selector("img")
+						page.screenshot(path="pics/{}.png".format(u))
+						browser.close()
 
-				logging.info("{} Saved".format(u));
-				wait_time = random.randint(15, 65);
-				logging.info("Waiting: {} Sec".format(wait_time));
-				time.sleep(wait_time);
+					msg = "{} Saved".format(u);
+					logging.info(msg);
+					print(msg, flush=True);
+					wait_time = random.randint(15, 65);
+					msg = "Waiting: {} Sec".format(wait_time);
+					logging.info(msg);
+					print(msg, flush=True);
+					time.sleep(wait_time);
 
-			else:
-				logging.info("Skipping: {}".format(u));
+				else:
+					msg = "Skipping: {}".format(u);
+					logging.info(msg);
+					print(msg, flush=True);
 
-			mydb = mysql.connector.connect(**DBconfig);
-			mycursor = mydb.cursor(dictionary=True);
-			mycursor.execute("SELECT * FROM `settings` WHERE `settings_name` = 'bio_snapshot'");
-			myresult = mycursor.fetchone();
-			mydb.close();
-			cont_loop = (myresult["settings_value"] == 1);
-			if not cont_loop:
-				break;
+				mydb = mysql.connector.connect(**DBconfig);
+				mycursor = mydb.cursor(dictionary=True);
+				mycursor.execute("SELECT * FROM `settings` WHERE `settings_name` = 'bio_snapshot'");
+				myresult = mycursor.fetchone();
+				mydb.close();
+				cont_loop = (myresult["settings_value"] == 1);
+				if not cont_loop:
+					break;
+		else:
+			break;
 
-		if i == len(all_user_results)-1:
-			cont_loop = False;
-		
 except Exception as e:
 	logging.debug("----------");
 	logging.error(e);
 	logging.debug("----------");
+	print(e, flush=True);
 	while CONTINUE_LOOP:
-		print("Debug mode");
+		print("Debug mode", flush=True);
 		time.sleep(60);
