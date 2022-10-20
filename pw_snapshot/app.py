d412cf5e2f016d0743d80b991ad93c30c34d9339
Mark Garcia <mymgcode@gmail.com>
2022-10-20 15:21:00 -0500

    sandbox, store webpage
    created a sandbox, storing cache, and updated needed folders

--- pw_snapshot/app.py
+++ pw_snapshot/app.py
@@ -9,17 +9,16 @@ import logging
 import requests
 import io
 from PIL import Image
-
-if not os.path.exists("log"):
-	os.mkdir('log');
-if not os.path.exists("pics"):
-	os.mkdir('pics');
-if not os.path.exists("pics/shots"):
-	os.mkdir('pics/shots');
-if not os.path.exists("pics/shots"):
-	os.mkdir('pics/shots');
-if not os.path.exists("pics/pfp"):
-	os.mkdir('pics/pfp');
+required_folders = {
+	"log":"log",
+	"pics":"pics",
+	"shots":"pics/shots",
+	"pfp":"pics/pfp",
+	"cache":"cache"
+}
+for dir_var in required_folders:
+	if not os.path.exists(required_folders[dir_var]):
+		os.mkdir(required_folders[dir_var]);
 
 logging.basicConfig(filename="log/default.log");
 logging.getLogger().setLevel(logging.DEBUG);
@@ -37,19 +36,18 @@ KEEP_ALIVE = False;
 def hide_elem_JSxpath(pg, current_xpath):
 	pg.evaluate("""() =>  document.evaluate(`{}`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none" """.format(current_xpath));
 
+def show_msg(msg):
+	logging.info(msg);
+	print(msg, flush=True);
 
 def img_down(link, filename):
 	response  = requests.get(link).content 
 	image_file = io.BytesIO(response)
 	image  = Image.open(image_file).convert('RGB')
 	split_tup = os.path.splitext(link);
-	msg = "pics/pfp/{}{}".format(filename, str(split_tup[1]));
-	logging.info(msg);
-	print(msg, flush=True);
-	image.save("pics/pfp/{}{}".format(filename, str(split_tup[1])) , "JPEG");
-	msg = "Downladed PFP";
-	logging.info(msg);
-	print(msg, flush=True);
+	show_msg( "{}/{}{}".format(required_folders["pfp"], filename, str(split_tup[1])) );
+	image.save("{}/{}{}".format(required_folders["pfp"], filename, str(split_tup[1])) , "JPEG");
+	show_msg("Downladed PFP");
 
 try:
 	time.sleep(5)
@@ -60,13 +58,16 @@ try:
 	mydb.close();
 	cont_loop = (myresult["settings_value"] == 1);
 	while cont_loop:
-		msg = "Bio_snapshot: On";
-		logging.info(msg);
-		print(msg, flush=True);
+		show_msg("Bio_snapshot: On");
 		time.sleep(1)
 		mydb = mysql.connector.connect(**DBconfig);
 		mycursor = mydb.cursor(dictionary=True);
-		mycursor.execute("SELECT `t_username` FROM `twitter`");
+		mycursor.execute("SELECT `t_username` FROM `twitter_skip` ORDER BY `ts_id` ASC;");
+		skip_list = [list(x.values())[0] for x in mycursor.fetchall()] + [x.replace(".html","") for x in os.listdir(required_folders["cache"])];
+		sql_not = "";
+		if len(skip_list):
+			sql_not = "WHERE `t_username` NOT IN ({})".format(", ".join(["'{}'".format(x) for x in skip_list]));
+		mycursor.execute("SELECT `t_username` FROM `twitter` {} ORDER BY `twit_id` ASC;".format(sql_not));
 		all_user_results = mycursor.fetchall();
 		mydb.close();
 		if len(all_user_results):
@@ -74,51 +75,45 @@ try:
 				u = x["t_username"];
 				if u == "":
 					continue;
-				if not os.path.exists("pics/shots/{}.png".format(u)):
-					with sync_playwright() as p:
-						browser = p.chromium.launch()
-						page = browser.new_page()
-						msg = "{}/{} - Opening https://twitter.com/{}".format(i+1,len(all_user_results),u);
-						logging.info(msg);
-						print(msg, flush=True);
-						page.goto("https://twitter.com/{}".format(u));
-						page.wait_for_selector('img');
-						img_elem = page.query_selector('[href="/{}/photo"] img'.format(u));
-						if img_elem is not None:
-							img_down(img_elem.get_attribute('src'), u);
+				with sync_playwright() as p:
+					browser = p.chromium.launch();
+					page = browser.new_page();
+					show_msg("{}/{} - Opening https://twitter.com/{}".format(i+1,len(all_user_results),u));
+					page.goto("https://twitter.com/{}".format(u));
+					page.wait_for_selector('img');
+					img_elem = page.query_selector('[href="/{}/photo"] img'.format(u));
+					if img_elem is not None:
+						img_down(img_elem.get_attribute('src'), u);
+					else:
+						if page.query_selector("span :text('Yes, view profile')"):
+							show_msg("PFP Blocked");
+							page.locator("span :text('Yes, view profile')").click()
+							page.mouse.wheel(0, -15000)
 						else:
-							if page.query_selector("span :text('Yes, view profile')"):
-								msg = "PFP Blocked";
-								logging.info(msg);
-								print(msg, flush=True);
-								page.locator("span :text('Yes, view profile')").click()
-								page.mouse.wheel(0, -15000)
-							else:
-								msg = "PFP Missing";
-								logging.info(msg);
-								print(msg, flush=True);
-							img_elem = page.query_selector('[href="/{}/photo"] img'.format(u));
-							img_down(img_elem.get_attribute('src'), u);
+							show_msg("PFP Missing");
+							mydb = mysql.connector.connect(**DBconfig);
+							mycursor = mydb.cursor(dictionary=True, buffered=True);
+							sql_query = f"INSERT INTO `twitter_skip` (`ts_id`, `t_username`) VALUES (NULL, %(t_username)s)";
+							mycursor.execute(sql_query, {"t_username": u});
+							mydb.commit();
+							mydb.close();
+							continue;
+						img_elem = page.query_selector('[href="/{}/photo"] img'.format(u));
+						img_down(img_elem.get_attribute('src'), u);
 
-						hide_elem_JSxpath(page, """//*[@id="layers"]/div""");
-						hide_elem_JSxpath(page, """//*[@id="react-root"]/div/div/div[2]/header/div""");
-						hide_elem_JSxpath(page, """//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[3]""");
-						page.screenshot(path="pics/shots/{}.png".format(u))
-						browser.close()
+					hide_elem_JSxpath(page, """//*[@id="layers"]/div""");
+					hide_elem_JSxpath(page, """//*[@id="react-root"]/div/div/div[2]/header/div""");
+					hide_elem_JSxpath(page, """//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[3]""");
+					page.screenshot(path="{}/{}.png".format(required_folders["shots"], u))
+					html_str = page.query_selector('html').inner_html();
+					with open("{}/{}.html".format(required_folders["cache"], u), "w") as text_file:
+						text_file.write(html_str)
+					browser.close()
 
-					msg = "{} Saved".format(u);
-					logging.info(msg);
-					print(msg, flush=True);
-					wait_time = random.randint(15, 65);
-					msg = "Waiting: {} Sec".format(wait_time);
-					logging.info(msg);
-					print(msg, flush=True);
-					time.sleep(wait_time);
-
-				else:
-					msg = "{}/{} - Skipping: {}".format(i+1,len(all_user_results),u);
-					logging.info(msg);
-					print(msg, flush=True);
+				show_msg("{} Saved".format(u));
+				wait_time = random.randint(15, 65);
+				show_msg("Waiting: {} Sec".format(wait_time));
+				time.sleep(wait_time);
 
 				mydb = mysql.connector.connect(**DBconfig);
 				mycursor = mydb.cursor(dictionary=True);
@@ -127,16 +122,12 @@ try:
 				mydb.close();
 				cont_loop = (myresult["settings_value"] == 1);
 				if not cont_loop:
-					msg = "Quiting";
-					logging.info(msg);
-					print(msg, flush=True);
+					show_msg("Quiting");
 					break;
 		else:
 			break;
 
-	msg = "Done";
-	logging.info(msg);
-	print(msg, flush=True);
+	show_msg("Done");
 
 except Exception as e:
 	logging.debug("----------");
---
+++ sandbox/exec.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker exec -it simple-api-sandbox-1 /bin/bash
