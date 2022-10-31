07370720b0915ed9ced432efdd57e2cd3aeba2f7
Mark Garcia <mymggithub@users.noreply.github.com>
2022-10-30 20:32:59 -0600

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 docker-compose stop
-docker rm simple-api-pw_snapshot-1
+docker rm pw-snapshot
+docker rm simple-api-apache-ui-1
 docker rm simple-api-phpmyadmin-1
-docker rm simple-api-mysql_db-1
+docker rm simple-api-mysql-db-1
 docker rm simple-api-flask-api-1
-docker rmi simple-api-pw_snapshot
+docker rmi simple-api-pw-snapshot
 docker rmi simple-api-flask-api
 docker rmi phpmyadmin
 docker rmi mysql
-docker volume rm simple-api_pw_log
-docker volume rm simple-api_api_log
-docker volume rm simple-api_db_data
+docker volume rm simple-api_pw-log
+docker volume rm simple-api_api-log
+docker volume rm simple-api_db-data
